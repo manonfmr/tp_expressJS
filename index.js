bb07d253e1ee7b21acdd8cc408e71c11b3aee87a
Manon <famerym@3il.fr>
2024-04-25 10:51:18 +0200

    ajax connexion et index

--- index.js
+++ index.js
@@ -28,28 +28,56 @@ app.use(sessions({
 
 app.use(express.urlencoded({extended: true}));
 
-baseDeDonnee.connect(function(err, next){
+baseDeDonnee.connect(function(err, next) {
     if (err) next(err);
-    app.post('/index.html', function(req, res){
-        if(err) console.log("erreur");
+
+    app.post('/ajaxSession.html', function(req, res) {
+        console.log("ajaxSession")
         let login = req.body.login;
-        let sql = 'SELECT * FROM personne where login = ? ;';
-        baseDeDonnee.query(sql, [login], function(err, resultat){
+        if (login) {
+            let sql = 'SELECT * FROM personne where login = ? ;';
+            baseDeDonnee.query(sql, [login], function(err, resultat) {
+                if (err) next(err);
+                if (resultat.length === 0) {
+                    res.json({ success: false, login: undefined });
+                } else {
+                    req.session.login = login;
+                    res.json({ success: true, login: login });
+                }
+            });
+        }
+    });
+
+    app.post('/ajaxAnnonce.html', function(req, res) {
+        console.log("ajaxAnnonce")
+        if (err) console.log("erreur ajaxAnnonce.html");
+        let sql = 'SELECT * FROM annonce';
+        let tabAnnonceNom = [];
+        let tabAnnonceLieu = [];
+        let tabAnnonceDescription = [];
+        baseDeDonnee.query(sql, function(err, resultat) {
+            console.log("bd annonce ")
             if (err) next(err);
-            if (resultat.length === 0){
-                res.redirect('/connexion.html');
+            if (resultat.length === 0) {
+                console.log("erreur donnée ")
+                res.json({ success: false });
+            } else {
+                for (let i = 0; i < resultat.length; i++) {
+                    tabAnnonceNom.push(resultat[i].nom);
+                    tabAnnonceLieu.push(resultat[i].lieu);
+                    tabAnnonceDescription.push(resultat[i].description);
+                }
+                res.json({ success: true, tabAnnonceNom: tabAnnonceNom, tabAnnonceLieu: tabAnnonceLieu, tabAnnonceDescription: tabAnnonceDescription });
             }
-            else{
-                req.session.login = login;
-                fileSystem.readFile(__dirname + '/index.html', 'utf8', function(err, data) {
-                    if (err) next(err);
-                    data = data.replace('{{login}}', login);
-                    res.send(data);
-                });
-            }
-        });       
-    })
+
+        });
+    });
 });
+
+
+app.get('/index.html', function(req, res){
+    res.sendFile(__dirname+'/index.html');
+})
 
 app.get('/connexion.html', function(req, res){
     res.sendFile(__dirname+'/connexion.html');
